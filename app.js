ab6eb896a8ac42643c75d33ea677dab8cd4aef58
igoryen <igor.yentaltsev@gmail.com>
2015-12-27 12:10:26 -0500

    9) added /view/, image handling

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var routes  = require( "./routes" );
-
 var nunjucks = require( 'nunjucks' );
-nunjucks.configure('public', {
+
+nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
 
+app.use(express.static('public'));
+
 // routes
+
 app.get("/", routes.pages("index"));
 
 app.listen(1976);

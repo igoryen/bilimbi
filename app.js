6c9ae8800d676f028da2fa32ddd4686b8d689a56
igoryen <igor.yentaltsev@gmail.com>
2015-12-27 10:11:14 -0500

    5) add public/index.html

--- app.js
+++ app.js
@@ -4,12 +4,5 @@ var path = require('path');
 
 app.use(express.static(path.join(__dirname + "/public")));
 
-app.get('/', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
-});
-
 app.listen(1976);
 console.log('Listening on port 1976');
